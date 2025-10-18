77c3d8f0ec44eaceaaac654143ef2dc2cb3adba5
CB331-Laser <zhenggroup@hotmail.com>
2025-10-18 20:39:56 +0800

    fix bugs: return the orign return when wrapped by the ignore_correction_error

--- labctrl/components/linear_stages/servo/utils.py
+++ labctrl/components/linear_stages/servo/utils.py
@@ -5,7 +5,8 @@ def ignore_connection_error(func):
     @wraps(func)
     def ret(*args, **kwargs):
         try:
-            func(*args, **kwargs)
+            res = func(*args, **kwargs)
+            return res
         except requests.exceptions.ConnectionError as e:
             print("Connection failed")
     return ret
